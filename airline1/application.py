ba7c50dd43dcf190cefe7208e2b61b49a8ea26aa
Saed Yousef <saed.alzaben@gmail.com>
2020-04-28 02:59:53 +0300

    a very simple API

--- airline1/application.py
+++ airline1/application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from models import *
 
 app = Flask(__name__)
@@ -46,4 +46,23 @@ def flight(flight_id):
     #passengers = Passenger.query.filter_by(flight_id = flight_id).all()
     passengers = flight.passengers
     return render_template("flight.html", flight=flight, passengers=passengers)
+    
+@app.route("/api/flights/<int:flight_id>")
+def flight_api(flight_id):
+    """List Details for a flight"""
+    # Make sure flight exists
+    flight = Flight.query.get(flight_id)
+    if flight is None:
+        return jsonify({"error" : "Invalid flight_id"}), 422
+    # Get all passengers
+    passengers = flight.passengers
+    names = []
+    for passenger in passengers:
+        names.append(passenger.name)
+    return jsonify({
+        "origin" : flight.origin,
+        "destination" : flight.destination,
+        "duration" : flight.duration,
+        "passengers" : names
+    })
     
